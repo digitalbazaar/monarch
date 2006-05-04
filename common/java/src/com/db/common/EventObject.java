a54c813d5fe5e06093e610acdf3bca75da2ec782
Dave Longley <dlongley@digitalbazaar.com>
2006-05-04 17:59:49 +0000

    Added BoxingHashMap as a convenience class. It is a HashMap that boxes primitives when storing them in the map and unboxes them when retrieving them.
    Used BoxingHashMap in EventObject.

--- common/java/src/com/db/common/EventObject.java
+++ common/java/src/com/db/common/EventObject.java
@@ -3,8 +3,6 @@
  */
 package com.db.common;
 
-import java.util.HashMap;
-
 /**
  * A generic event object for passing event data.
  * 
@@ -20,7 +18,7 @@ public class EventObject
    /**
     * A map of data bundled with this event.
     */
-   protected HashMap mDataMap;
+   protected BoxingHashMap mDataMap;
    
    /**
     * Creates a new EventObject with no specified name.
@@ -38,7 +36,7 @@ public class EventObject
    public EventObject(String name)
    {
       setName(name);
-      mDataMap = new HashMap();
+      mDataMap = new BoxingHashMap();
    }
 
    /**
@@ -76,10 +74,223 @@ public class EventObject
     * Gets a data value for a key for this event.
     * 
     * @param key the key for the data for this event.
+    * 
     * @return the data value for the passed key for this event.
     */
    public Object getData(Object key)
    {
       return mDataMap.get(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, byte value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public byte getDataByteValue(Object key)
+   {
+      return mDataMap.getByte(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, short value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public short getDataShortValue(Object key)
+   {
+      return mDataMap.getShort(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, int value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public int getDataIntValue(Object key)
+   {
+      return mDataMap.getInt(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, long value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public long getDataLongValue(Object key)
+   {
+      return mDataMap.getLong(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, float value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public float getDataFloatValue(Object key)
+   {
+      return mDataMap.getFloat(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, double value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public double getDataDoubleValue(Object key)
+   {
+      return mDataMap.getDouble(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, boolean value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public boolean getDataBooleanValue(Object key)
+   {
+      return mDataMap.getBoolean(key);
+   }
+   
+   /**
+    * Sets a key-value pair of data for this event.
+    * 
+    * @param key the key for the data for this event.
+    * @param value the value for the data for this event.
+    */
+   public void setData(Object key, char value)
+   {
+      mDataMap.put(key, value);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public char getDataCharValue(Object key)
+   {
+      return mDataMap.getChar(key);
+   }
+   
+   /**
+    * Gets a data value for a key for this event.
+    * 
+    * @param key the key for the data for this event.
+    * 
+    * @return the data value for the passed key for this event.
+    */
+   public String getDataStringValue(Object key)
+   {
+      return mDataMap.getString(key);
+   }
+   
+   /**
+    * Makes a copy of this event object.
+    * 
+    * @return a copy of this event object.
+    */
+   public EventObject copy()
+   {
+      // copy name
+      EventObject copy = new EventObject(getName());
+      
+      // copy data map
+      copy.mDataMap = mDataMap.copy();
+      
+      return copy;
    }
 }
