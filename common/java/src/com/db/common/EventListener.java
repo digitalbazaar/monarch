e7945819c3f7fb5dfb1e23b6b4d774b84db317af
Dave Longley <dlongley@digitalbazaar.com>
2006-05-05 14:22:33 +0000

    Added a generic event listener for convenience.

---
+++ common/java/src/com/db/common/EventListener.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006 Digital Bazaar, Inc.  All rights reserved.
+ */
+package com.db.common;
+
+/**
+ * A generic event listener.
+ * 
+ * @author Dave Longley
+ */
+public interface EventListener
+{
+   /**
+    * Called when an event occurs.
+    * 
+    * @param event the event that occurred.
+    */
+   public void eventOccurred(EventObject event);
+}
