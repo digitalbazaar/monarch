178fdbfb207539cccbf61e58e605517194343979
Dave Longley <dlongley@digitalbazaar.com>
2006-06-13 17:20:57 +0000

    Added commented out code for turning on anti-aliasing if desired for close buttons.

--- gui/java/src/com/db/gui/EditableTabPanel.java
+++ gui/java/src/com/db/gui/EditableTabPanel.java
@@ -16,6 +16,7 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Rectangle;
+//import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -290,8 +291,12 @@ public class EditableTabPanel extends TabPanel
       
       BasicStroke bs = new BasicStroke(1);
       
-      // draw the x
+      // create the graphics
       Graphics2D g2 = image.createGraphics();
+      //g2.setRenderingHint(
+         //RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      
+      // draw the x
       g2.setColor(Color.black);
       g2.setStroke(bs);
       g2.draw(path);
@@ -332,8 +337,12 @@ public class EditableTabPanel extends TabPanel
       
       BasicStroke bs = new BasicStroke(1);
       
-      // draw the x outline
+      // create the graphics
       Graphics2D g2 = image.createGraphics();
+      //g2.setRenderingHint(
+         //RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);      
+      
+      // draw the x outline
       g2.setColor(Color.black);
       g2.setStroke(bs);
       g2.draw(path);
@@ -387,8 +396,12 @@ public class EditableTabPanel extends TabPanel
       
       BasicStroke bs = new BasicStroke(1);
       
-      // draw the x outline
+      // create the graphics
       Graphics2D g2 = image.createGraphics();
+      //g2.setRenderingHint(
+         //RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      
+      // draw the x outline
       g2.setColor(Color.black);
       g2.setStroke(bs);
       g2.draw(path);
