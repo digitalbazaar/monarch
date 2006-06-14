bf84f8eeaf2f82e2110bbda870068812d988cf92
Dave Longley <dlongley@digitalbazaar.com>
2006-06-14 17:47:40 +0000

    Added new progress bar that is intended to adjust itself more quickly than a JProgressBar.

---
+++ gui/java/src/com/db/gui/FastProgressBar.java
@@ -0,0 +1,407 @@
+/*
+ * Copyright (c) 2006 Digital Bazaar, Inc.  All rights reserved.
+ */
+package com.db.gui;
+
+import javax.swing.JPanel;
+
+import com.db.util.MethodInvoker;
+
+/**
+ * This class represents a progress bar that can adjust and redraw
+ * itself quickly.
+ * 
+ * @author Dave Longley
+ */
+public class FastProgressBar extends JPanel
+{
+   /**
+    * The minimum value for this progress bar.
+    */
+   protected int mMinimumValue;
+   
+   /**
+    * The maximum value for this progress bar.
+    */
+   protected int mMaximumValue;
+   
+   /**
+    * The current value for this progress bar.
+    */
+   protected int mValue;
+   
+   /**
+    * Whether or not this progress bar is set to indeterminate mode.
+    */
+   protected boolean mIndeterminate;
+   
+   /**
+    * The current indeterminate value. This value bounces between the minimum
+    * and maximum values when the progress bar is set in indeterminate mode.
+    */
+   protected int mIndeterminateValue;
+   
+   /**
+    * The indeterminate time interval. This interval is used to cycle
+    * progress when this progress bar's definite value is unknown.
+    */
+   protected long mIndeterminateInterval;
+   
+   /**
+    * The text to display in the progress bar.
+    */
+   protected String mText;
+   
+   /**
+    * The thread used to cycle progress.
+    */
+   protected Thread mProgressCyclerThread;
+   
+   /**
+    * The ui class id for this component.
+    */
+   protected static final String mUiClassId = "com.db.gui.FastProgressBarUI";
+   
+   /**
+    * Creates a new FastProgressBar.
+    */
+   public FastProgressBar()
+   {
+      // set value range and value
+      setMinimumValue(0);
+      setMaximumValue(100);
+      setValue(0);
+      
+      // set text
+      setText(null);
+      
+      // initialize indeterminate values
+      setIndeterminate(false);
+      setIndeterminateValue(0);
+      setIndeterminateInterval(50);
+      
+      // initialize progress cycler thread to null
+      mProgressCyclerThread = null;
+   }
+   
+   /**
+    * Starts cycling progress in a background process.
+    */
+   protected void startCyclingProgress()
+   {
+      // execute cycle progress in a background process 
+      MethodInvoker mi = new MethodInvoker(this, "cycleProgress", null);
+      mi.backgroundExecute();
+   }
+   
+   /**
+    * Stops cycling progress. 
+    */
+   protected void stopCyclingProgress()
+   {
+      // interrupt current progress cycling thread if one exists
+      if(mProgressCyclerThread != null)
+      {
+         mProgressCyclerThread.interrupt();
+      }
+   }
+   
+   /**
+    * This method cycles the progress on this progress bar when it is
+    * in indeterminate mode.
+    */
+   public void cycleProgress()
+   {
+      try
+      {
+         // set indeterminate value to minimum value
+         setIndeterminateValue(getMinimumValue());
+         
+         // progress starts out as increasing
+         boolean increasing = true;
+         
+         // cycles progress while this progress bar is set to indeterminate
+         while(isIndeterminate() && !Thread.interrupted())
+         {
+            // sleep for indeterminate interval
+            Thread.sleep(getIndeterminateInterval());
+            
+            // if still indeterminate and not interrupted, proceed
+            if(isIndeterminate() && !Thread.interrupted())
+            {
+               // repaint
+               repaint();
+
+               // get old indeterminate value
+               int value = getIndeterminateValue();
+               
+               // make sure value is in range
+               value = Math.min(getMaximumValue(), value);
+               value = Math.max(getMinimumValue(), value);
+               
+               // set new value
+               if(increasing)
+               {
+                  value++;
+               }
+               else
+               {
+                  value--;
+               }
+               
+               setIndeterminateValue(value);
+
+               // set increasing or decreasing progress 
+               if(value == getMaximumValue())
+               {
+                  increasing = false;
+               }
+               else if(value == getMinimumValue())
+               {
+                  increasing = true;
+               }
+            }
+         }
+      }
+      catch(InterruptedException ignore)
+      {
+         // ignore interrupted exception
+      }
+      
+      // repaint once more
+      repaint();
+   }
+   
+   /**
+    * Resets the UI property with a value from the current look and feel.
+    */
+   public void updateUI()
+   {
+      setUI(new FastProgressBarUI(this));
+   }
+   
+   /**
+    * Returns a string that specifies the name of the L&F class that
+    * renders this component.
+    *
+    * @return the UIClassID for this component.
+    */
+   public String getUIClassID()
+   {
+      return mUiClassId;
+   }
+   
+   /**
+    * Gets the fast progress bar ui for this fast progress bar.
+    * 
+    * @return the fast progress bar ui for this fast progress bar.
+    */
+   public FastProgressBarUI getFastProgressBarUI()
+   {
+      return (FastProgressBarUI)getUI();
+   }
+   
+   /**
+    * Sets the minimum value for this progress bar. 
+    * 
+    * @param min the minimum value for this progress bar.
+    */
+   public void setMinimumValue(int min)
+   {
+      mMinimumValue = min;
+   }
+   
+   /**
+    * Gets the minimum value for this progress bar. 
+    * 
+    * @return the minimum value for this progress bar.
+    */
+   public int getMinimumValue()
+   {
+      return mMinimumValue;
+   }
+   
+   /**
+    * Sets the maximum value for this progress bar.
+    * 
+    * @param max the maximum value for this progress bar.
+    */
+   public void setMaximumValue(int max)
+   {
+      mMaximumValue = max;
+   }
+   
+   /**
+    * Gets the maximum value for this progress bar.
+    * 
+    * @return the maximum value for this progress bar.
+    */
+   public int getMaximumValue()
+   {
+      return mMaximumValue;
+   }
+   
+   /**
+    * Sets the current value for this progress bar.
+    * 
+    * @param value the current value for this progress bar. 
+    */
+   public void setValue(int value)
+   {
+      mValue = value;
+   }
+   
+   /**
+    * Gets the current value for this progress bar.
+    * 
+    * @return the current value for this progress bar. 
+    */
+   public int getValue()
+   {
+      return mValue;
+   }
+   
+   /**
+    * Gets the range of values for this progress bar.
+    * 
+    * @return the range of values for this progress bar.
+    */
+   public long getRange()
+   {
+      return getMaximumValue() - getMinimumValue();
+   }
+   
+   /**
+    * Gets the percentage the current value is out of the range of values. The
+    * value that is returned is between 0.0 and 1.0.
+    * 
+    * @return the percentage of the range of values that the current value is.
+    */
+   public double getPercentage()
+   {
+      double rval = 0.0;
+      
+      double value = getValue();
+      rval = value / getRange();
+      
+      return rval;
+   }
+
+   /**
+    * Sets whether or not this progress bar is in indeterminate mode. 
+    * 
+    * @param indeterminate true to set this progress bar to indeterminate
+    *                      mode, false to set this progress bar to
+    *                      determinate mode.
+    */
+   public synchronized void setIndeterminate(boolean indeterminate)
+   {
+      if(mIndeterminate != indeterminate)
+      {
+         mIndeterminate = indeterminate;
+         
+         if(indeterminate)
+         {
+            // start cycling progress
+            startCyclingProgress();
+         }
+         else
+         {
+            // stop cycling progress
+            stopCyclingProgress();
+         }
+      }
+   }
+   
+   /**
+    * Gets whether or not this progress bar is in indeterminate mode. 
+    * 
+    * @return true to set this progress bar to indeterminate mode, false
+    *         false to set this progress bar to determinate mode.
+    */
+   public synchronized boolean isIndeterminate()
+   {
+      return mIndeterminate;
+   }
+   
+   /**
+    * Sets the current indeterminate value.
+    * 
+    * @param value the current indeterminate value.
+    */
+   public void setIndeterminateValue(int value)
+   {
+      mIndeterminateValue = value;
+   }
+   
+   /**
+    * Gets the current indeterminate value.
+    * 
+    * @return the current indeterminate value.
+    */
+   public int getIndeterminateValue()
+   {
+      return mIndeterminateValue;
+   }
+   
+   /**
+    * Sets the indeterminate interval. This interval is used to cycle
+    * progress when this progress bar's definite value is unknown.
+    * 
+    * @param interval the indeterminate interval.
+    */
+   public void setIndeterminateInterval(long interval)
+   {
+      mIndeterminateInterval = interval;      
+   }
+   
+   /**
+    * Gets the indeterminate interval. This interval is used to cycle
+    * progress when this progress bar's definite value is unknown.
+    * 
+    * @return the indeterminate interval.
+    */
+   public long getIndeterminateInterval()
+   {
+      return mIndeterminateInterval;      
+   }
+   
+   /**
+    * Gets the percentage the indeterminate value is out of the range of
+    * values. The value that is returned is between 0.0 and 1.0.
+    * 
+    * @return the percentage of the range of values that the indeterminate
+    *         value is.
+    */
+   public double getIndeterminatePercentage()
+   {
+      double rval = 0.0;
+      
+      double value = getIndeterminateValue();
+      rval = value / getRange();
+      
+      return rval;
+   }
+   
+   /**
+    * Sets the text to display in the progress bar. If null is passed then
+    * no text will be displayed in the progress bar.
+    * 
+    * @param text the text to display in the progress bar.
+    */
+   public void setText(String text)
+   {
+      mText = text;
+   }
+   
+   /**
+    * Gets the text displayed in the progress bar.
+    * 
+    * @return the text displayed in the progress bar or null if no text is
+    *         displayed.
+    */
+   public String getText()
+   {
+      return mText;
+   }
+}
---
+++ gui/java/src/com/db/gui/FastProgressBarUI.java
@@ -0,0 +1,466 @@
+/*
+ * Copyright (c) 2006 Digital Bazaar, Inc.  All rights reserved.
+ */
+package com.db.gui;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.Rectangle;
+
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.plaf.ComponentUI;
+import javax.swing.plaf.PanelUI;
+
+import com.sun.java.swing.SwingUtilities2;
+
+/**
+ * This class draws the ui for a FastProgressBar.
+ * 
+ * @author Dave Longley
+ */
+public class FastProgressBarUI extends PanelUI
+{
+   /**
+    * Shared ui object.
+    */
+   protected static FastProgressBarUI smFastProgressBarUI;
+   
+   /**
+    * The meter color.
+    */
+   protected Color mMeterColor;
+   
+   /**
+    * The empty meter text color.
+    */
+   protected Color mEmptyMeterTextColor;
+   
+   /**
+    * The full meter text color.
+    */
+   protected Color mFullMeterTextColor;
+   
+   /**
+    * Creates a new FastProgressBarUI.
+    */
+   public FastProgressBarUI()
+   {
+   }
+   
+   /**
+    * Creates a new FastProgressBarUI for a specific fast progress bar.
+    * 
+    * @param progressBar the progress bar to create the ui for.
+    */
+   public FastProgressBarUI(FastProgressBar progressBar)
+   {
+      // install defaults
+      installDefaults(progressBar);
+   }
+
+   /**
+    * Creates a new FastProgressBarUI for the passed component.
+    * 
+    * @param c the component to create the ui for.
+    */
+   public static ComponentUI createUI(JComponent c)
+   {
+      if(smFastProgressBarUI == null)
+      {
+         smFastProgressBarUI = new FastProgressBarUI();
+      }
+      
+      return smFastProgressBarUI;
+   }
+
+   /**
+    * Overridden to call install ui defaults on a progress bar.
+    * 
+    * @param p the panel to install ui defaults on.
+    */
+   protected void installDefaults(JPanel p)
+   {
+      installDefaults((FastProgressBar)p);
+   }
+   
+   /**
+    * Installs default values for this ui onto the passed progress bar.
+    * 
+    * @param progressBar the progressBar to install defaults on.
+    */
+   protected void installDefaults(FastProgressBar progressBar)
+   {
+      LookAndFeel.installProperty(progressBar, "opaque", Boolean.TRUE);
+      LookAndFeel.installBorder(progressBar,"ProgressBar.border");
+      LookAndFeel.installColorsAndFont(progressBar,
+         "ProgressBar.background", "ProgressBar.foreground",
+         "ProgressBar.font");
+      
+      // reset text and meter colors
+      resetTextAndMeterColors();
+   }
+   
+   /**
+    * Gets the bounding box for painting the progress meter.
+    * 
+    * @param progressBar the progress bar to paint the meter for.
+    * 
+    * @return the bounding box for painting the progress meter.
+    */
+   protected Rectangle getProgressBarBounds(FastProgressBar progressBar)
+   {
+      Rectangle rect = progressBar.getBounds();
+
+      // get insets
+      Insets insets = progressBar.getInsets();
+      
+      // subtract insets
+      rect.x += insets.left;
+      rect.y += insets.top;
+      rect.width -= insets.right;
+      rect.height -= insets.bottom;
+      
+      return rect;
+   }
+   
+   /**
+    * Gets the indeterminate box bounds for the passed progress bar.
+    * 
+    * @param progressBarBounds the bounds for the progress bar.
+    * @param percentage the percentage of the indeterminate value for the
+    *                   progress bar.
+    * 
+    * @return the indeterminate box bounds.
+    */
+   protected Rectangle getBoxBounds(
+      Rectangle progressBarBounds, double percentage)
+   {
+      Rectangle rect = new Rectangle();
+      
+      // set the width and height of the box
+      rect.width = progressBarBounds.width / 10;  
+      rect.height = progressBarBounds.height;
+      
+      // map the indeterminate value to the progress bar bounds less
+      // the size of the box and store it as the box's x coordinate
+      rect.x = (int)Math.round(percentage *
+         (progressBarBounds.width - rect.width));
+      
+      return rect;
+   }
+
+   /**
+    * Gets the determinate meter bounds for the passed progress bar.
+    * 
+    * @param progressBarBounds the bounds for the progress bar.
+    * @param percentage the percentage of the current value for the
+    *                   progress bar.
+    * 
+    * @return the determinate meter bounds.
+    */
+   protected Rectangle getMeterBounds(
+      Rectangle progressBarBounds, double percentage)
+   {
+      Rectangle rect = new Rectangle();
+      
+      // set the height of the meter
+      rect.height = progressBarBounds.height;
+      
+      // map the current value to the progress bar bounds and store
+      // it as the meter's width
+      rect.width = (int)Math.round(percentage * progressBarBounds.width);
+      
+      return rect;
+   }
+   
+   /**
+    * Paints this ui for the passed progress bar. The ui will paint
+    * an indeterminate progress box.
+    * 
+    * @param g the graphics to paint with.
+    * @param progressBar the progress bar to paint for.
+    */
+   protected void paintIndeterminateBox(
+      Graphics2D g, FastProgressBar progressBar)
+   {
+      // get the progress bar bounds
+      Rectangle progressBarBounds = progressBar.getBounds();
+      
+      // get the percentage
+      double percentage = progressBar.getIndeterminatePercentage();
+      
+      // paint indeterminate box
+      Rectangle rect = getBoxBounds(progressBarBounds, percentage);
+      g.setColor(getMeterColor());
+      g.fill(rect);
+      
+      // paint text
+      paintText(progressBar, g, progressBarBounds, rect);
+   }
+   
+   /**
+    * Paints this ui for the passed progress bar. The ui will paint
+    * a determinate progress meter.
+    * 
+    * @param g the graphics to paint with.
+    * @param progressBar the progress bar to paint for.
+    */
+   protected void paintDeterminateMeter(
+      Graphics2D g, FastProgressBar progressBar)
+   {
+      // get the progress bar bounds
+      Rectangle progressBarBounds = progressBar.getBounds();
+      
+      // get the indeterminate percentage
+      double percentage = progressBar.getPercentage();
+      
+      // paint determinate meter
+      Rectangle rect = getMeterBounds(progressBarBounds, percentage);
+      g.setColor(getMeterColor());
+      g.fill(rect);
+      
+      // paint text
+      paintText(progressBar, g, progressBarBounds, rect);
+   }
+   
+   /**
+    * Paints the text for the passed progress bar on top of an existing
+    * indeterminate box or determinate meter.
+    * 
+    * @param progressBar the progress bar to paint the text for.
+    * @param g the graphics to paint with. 
+    * @param progressBarBounds the bounds for the progress bar.
+    * @param boxOrMeterBounds the bounds for the existing box or meter.
+    */
+   protected void paintText(
+      FastProgressBar progressBar, Graphics2D g,
+      Rectangle progressBarBounds, Rectangle boxOrMeterBounds)
+   {
+      // get text, ensure it is not null
+      String text = progressBar.getText();
+      if(text != null)
+      {
+         // get the old clipping bounds
+         Rectangle oldClipBounds = g.getClipBounds();
+         
+         // set font
+         g.setFont(progressBar.getFont());
+         
+         // get the text width
+         FontMetrics fontMetrics =
+            progressBar.getFontMetrics(progressBar.getFont());
+         int textWidth = SwingUtilities2.stringWidth(
+            progressBar, fontMetrics, text);
+         
+         // get progress bar width and height
+         double width = progressBarBounds.width;
+         double height = progressBarBounds.height;
+            
+         // set the text x position
+         int x = (int)Math.round((width - textWidth) / 2);
+         
+         // set the text y position
+         int y = (int)Math.round(
+            (height - fontMetrics.getHeight()) / 2 + fontMetrics.getAscent());
+
+         // draw empty meter text
+         g.setColor(getEmptyMeterTextColor());
+         SwingUtilities2.drawString(progressBar, g, text, x, y);
+         
+         // set clipping bounds
+         g.setClip(boxOrMeterBounds);
+         
+         // draw full meter text
+         g.setColor(getFullMeterTextColor());
+         SwingUtilities2.drawString(progressBar, g, text, x, y);
+         
+         // restore the old clipping bounds
+         g.setClip(oldClipBounds);
+      }
+   }
+   
+   /**
+    * Paints this ui for the passed progress bar. Either paints it
+    * an indeterminate progress box or as a determinate progress meter.
+    * 
+    * @param g the graphics to paint with.
+    * @param progressBar the progress bar to paint.
+    */
+   public void paint(Graphics g, FastProgressBar progressBar)
+   {
+      // get 2d graphics
+      Graphics2D g2d = (Graphics2D)g;
+      
+      if(progressBar.isIndeterminate())
+      {
+         paintIndeterminateBox(g2d, progressBar);
+      }
+      else
+      {
+         paintDeterminateMeter(g2d, progressBar);
+      }
+   }
+   
+   /**
+    * Paints this ui for the passed component. The component will
+    * be cast to a FastProgressBar.
+    * 
+    * @param g the graphics to paint with.
+    * @param component the component to paint.
+    */
+   public void paint(Graphics g, JComponent component)
+   {
+      paint(g, (FastProgressBar)component);
+   }
+   
+   /**
+    * Gets the minimum size for the passed component.
+    * 
+    * @param c the component to get the minimum size for.
+    * 
+    * @return the component's minimum size.
+    */
+   public Dimension getMinimumSize(JComponent c)
+   {
+      return new Dimension(10, 10);
+   }
+   
+   /**
+    * Gets the maximum size for the passed component.
+    * 
+    * @param c the component to get the maximum size for.
+    * 
+    * @return the component's maximum size.
+    */
+   public Dimension getMaximumSize(JComponent c)
+   {
+      return new Dimension(Integer.MAX_VALUE, Integer.MAX_VALUE);
+   }
+   
+   /**
+    * Gets the preferred size for the passed component.
+    * 
+    * @param c the component to get the prefererd size for.
+    * 
+    * @return the component's preferred size.
+    */
+   public Dimension getPreferredSize(JComponent c)
+   {
+      Dimension size = getMinimumSize(c);
+      
+      // cast component to a progress bar
+      FastProgressBar progressBar = (FastProgressBar)c;
+      
+      // get font metrics for progress bar
+      FontMetrics fontMetrics =
+         progressBar.getFontMetrics(progressBar.getFont());
+      
+      // get text size
+      Dimension textSize = new Dimension(0, 0);
+      
+      // always set preferred height to text height
+      textSize.height = fontMetrics.getHeight();
+      
+      // set preferred width to text width, if there is text
+      if(progressBar.getText() != null) 
+      {
+         textSize.width = SwingUtilities2.stringWidth(
+            progressBar, fontMetrics, progressBar.getText());
+      }
+      
+      // increase size as necessary
+      size.width = Math.max(size.width, textSize.width);
+      size.height = Math.max(size.height, textSize.height);
+      
+      // get progress bar insets
+      Insets insets = progressBar.getInsets();
+      
+      // add insets
+      size.width += insets.left + insets.right;
+      size.height += insets.top + insets.bottom;
+      
+      return size;
+   }
+   
+   /**
+    * Sets the meter color.
+    * 
+    * @param color the meter color.
+    */
+   public void setMeterColor(Color color)
+   {
+      mMeterColor = color;
+   }
+
+   /**
+    * Gets the meter color.
+    * 
+    * @return the meter color.
+    */
+   public Color getMeterColor()
+   {
+      return mMeterColor;
+   }
+
+   /**
+    * Sets the empty meter text color.
+    * 
+    * @param color the empty meter text color.
+    */
+   public void setEmptyMeterTextColor(Color color)
+   {
+      mEmptyMeterTextColor = color;
+   }
+
+   /**
+    * Gets the empty meter text color.
+    * 
+    * @return the empty meter text color.
+    */
+   public Color getEmptyMeterTextColor()
+   {
+      return mEmptyMeterTextColor;
+   }
+   
+   /**
+    * Sets the full meter text color.
+    * 
+    * @param color the full meter text color.
+    */
+   public void setFullMeterTextColor(Color color)
+   {
+      mFullMeterTextColor = color;
+   }
+
+   /**
+    * Gets the full meter text color.
+    * 
+    * @return the full meter text color.
+    */
+   public Color getFullMeterTextColor()
+   {
+      return mFullMeterTextColor;
+   }
+   
+   /**
+    * Resets the text and meter colors to their default settings.
+    */
+   public void resetTextAndMeterColors()
+   {
+      // set the meter color
+      setMeterColor(UIManager.getColor("ProgressBar.selectionBackground"));
+      
+      // set the text colors
+      setEmptyMeterTextColor(
+         UIManager.getColor("ProgressBar.selectionBackground"));
+      setFullMeterTextColor(
+         UIManager.getColor("ProgressBar.selectionForeground"));
+   }
+}
