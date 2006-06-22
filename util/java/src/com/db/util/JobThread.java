559b2ffabe442b13e5b6fa5150d24c10347ec9d6
Dave Longley <dlongley@digitalbazaar.com>
2006-06-22 14:11:37 +0000

    Added JobThread and JobThreadPool classes to handle thread pooling.

---
+++ util/java/src/com/db/util/JobThread.java
@@ -0,0 +1,148 @@
+/*
+ * Copyright (c) 2006 Digital Bazaar, Inc.  All rights reserved.
+ */
+package com.db.util;
+
+import com.db.logging.Logger;
+import com.db.logging.LoggerManager;
+
+/**
+ * A JobThread is a thread that executes Runnable jobs and sleeps
+ * while it is not executing a job. This thread can be used in
+ * conjunction with a JobThreadPool.
+ * 
+ * @author Dave Longley
+ */
+public class JobThread extends Thread
+{
+   /**
+    * The Runnable job to execute.
+    */
+   protected Runnable mJob;
+   
+   /**
+    * Creates a new JobThread. 
+    */
+   public JobThread()
+   {
+      // no Runnable job to execute yet
+      mJob = null;
+   }
+   
+   /**
+    * Gets the Runnable job to execute, if any.
+    * 
+    * @return the Runnable job to execute, if any.
+    */
+   protected synchronized Runnable getJob()
+   {
+      return mJob;
+   }
+   
+   /**
+    * Sets the Runnable job for this thread. If null is passed then this
+    * thread will be considered idle (with no job).
+    * 
+    * @param job the job for this thread or null if this thread has no job.
+    */
+   public synchronized void setJob(Runnable job)
+   {
+      // set job
+      mJob = job;
+      
+      if(job == null)
+      {
+         // set thread name
+         setName("JobThread: idle");
+      }
+      else
+      {
+         // set thread name
+         setName("JobThread: running job '" + getJob() + "'");
+      }
+   }
+   
+   /**
+    * Runs this thread.
+    */
+   public void run()
+   {
+      try
+      {
+         while(!isInterrupted())
+         {
+            // if this thread has a job proceed
+            if(hasJob())
+            {
+               // get the Runnable job to run
+               Runnable job = getJob();
+               
+               try
+               {
+                  // run job
+                  job.run();
+               }
+               catch(Throwable t)
+               {
+                  // FIXME: only print stack trace at debug verbosity
+                  getLogger().error(
+                     "JobThread: An exception occurred " +
+                     "while running a job,\ntrace= " +
+                     LoggerManager.getStackTrace(t));
+                  //getLogger().debug(Logger.getStackTrace(t));
+               }
+               
+               // thread no longer has job
+               setJob(null);
+            }
+            
+            // sleep
+            Thread.sleep(1);
+         }
+      }
+      catch(InterruptedException e)
+      {
+         // ensure flag remains flipped
+         interrupt();
+         getLogger().debug("JobThread interrupted.");
+      }
+      catch(Throwable t)
+      {
+         // FIXME: only print stack trace at debug verbosity
+         getLogger().error(
+            "An exception occurred on a JobThread,\ntrace= " +
+            LoggerManager.getStackTrace(t));
+         //getLogger().debug(Logger.getStackTrace(t));
+      }
+   }
+   
+   /**
+    * Returns true if this thread has a job, false if it is idle.
+    * 
+    * @return true if this thread has a job, false if it is idle.
+    */
+   public synchronized boolean hasJob()
+   {
+      return getJob() != null;
+   }
+   
+   /**
+    * Returns true if this thread has no job, false if it does.
+    * 
+    * @return true if this thread has no job, false if it does.
+    */
+   public synchronized boolean isIdle()
+   {
+      return !hasJob();
+   }
+   
+   /**
+    * Gets the logger for this thread.
+    * 
+    * @return the logger for this thread.
+    */
+   public Logger getLogger()
+   {
+      return LoggerManager.getLogger("dbutil");
+   }
+}
---
+++ util/java/src/com/db/util/JobThreadPool.java
@@ -0,0 +1,284 @@
+/*
+ * Copyright (c) 2006 Digital Bazaar, Inc.  All rights reserved.
+ */
+package com.db.util;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+import com.db.logging.Logger;
+import com.db.logging.LoggerManager;
+import com.db.util.Semaphore;
+
+/**
+ * This class is a JobThread pool.
+ * 
+ * This pool maintains a set of N JobThreads that can be used to run jobs
+ * without having to tear down the threads and create new ones.
+ * 
+ * @author Dave Longley
+ */
+public class JobThreadPool
+{
+   /**
+    * This semaphore is used to regulate the number of threads that
+    * are available in this pool.
+    */
+   protected Semaphore mThreadSemaphore;
+   
+   /**
+    * The list of threads in this pool. This is the list of total threads
+    * in this pool.
+    */
+   protected Vector mThreads;
+   
+   /**
+    * Creates a new ThreadPool with 10 threads available for running jobs.
+    */
+   public JobThreadPool()
+   {
+      this(10);
+   }
+
+   /**
+    * Creates a new ThreadPool with the specified number of
+    * threads available for running jobs.
+    * 
+    * @param poolSize the size of the pool (number of threads).
+    */
+   public JobThreadPool(int poolSize)
+   {
+      // create the thread semaphore
+      mThreadSemaphore = new Semaphore(poolSize, true);
+      
+      // create the thread list
+      mThreads = new Vector();
+      
+      // set the pool size (number of threads)
+      setPoolSize(poolSize);
+   }
+   
+   /**
+    * Acquires a thread permit for running a job.
+    * 
+    * This method will lock until an available thread is acquired or
+    * the current thread is interrupted.
+    * 
+    * @throws InterruptedException
+    */
+   protected void acquireThreadPermit() throws InterruptedException
+   {
+      mThreadSemaphore.acquire();
+   }
+   
+   /**
+    * Releases a thread permit that was used to run a job.
+    */
+   protected void releaseThreadPermit()
+   {
+      mThreadSemaphore.release();
+   }
+   
+   /**
+    * Creates a new JobThread.
+    * 
+    * @return the new JobThread.
+    */
+   protected synchronized JobThread createJobThread()
+   {
+      return new JobThread();
+   }
+   
+   /**
+    * Gets an idle thread. This method will also clean up any extra
+    * idle threads that should not exist due to a decrease in the
+    * pool size. If an idle thread is not found, it will be created.
+    * 
+    * A thread permit should always be acquired before calling this
+    * method -- as to do otherwise will cause the number of threads to
+    * become out of sync with the thread semaphore permit count.
+    * 
+    * @return an idle thread.
+    */
+   protected synchronized JobThread getIdleThread()
+   {
+      JobThread rval = null;
+      
+      // get the number of extra threads
+      int extraThreads = mThreads.size() - mThreadSemaphore.getNumPermits();
+      
+      // iterate through threads, find one that is idle
+      Iterator i = mThreads.iterator();
+      while(i.hasNext())
+      {
+         JobThread thread = (JobThread)i.next();
+         if(thread.isIdle())
+         {
+            // if there are extra idle threads, interrupt and remove
+            // this one
+            if(extraThreads > 0)
+            {
+               // interrupt thread
+               thread.interrupt();
+               
+               // remove it from the pool
+               i.remove();
+               
+               // decrement extra threads
+               extraThreads--;
+            }
+            else
+            {
+               // return this thread
+               rval = thread;
+               break;
+            }
+         }
+      }
+      
+      // if no idle thread was found
+      if(rval == null)
+      {
+         // create new job thread
+         rval = createJobThread();
+         
+         // start the thread
+         rval.start();
+      }
+      
+      return rval;
+   }
+   
+   /**
+    * Runs the passed Runnable job on an idle JobThread.
+    * 
+    * @param job the Runnable job to run.
+    */
+   protected synchronized void runJobOnIdleThread(Runnable job)
+   {
+      // get an idle thread
+      JobThread thread = getIdleThread();
+      
+      // set job
+      thread.setJob(job);
+   }
+   
+   /**
+    * Sets the number of threads in this thread pool.
+    * 
+    * @param size the number of threads in this thread pool. It must be
+    *             a positive number.
+    */
+   public synchronized void setPoolSize(int size)
+   {
+      // disallow any size that is non-positive
+      if(size <= 0)
+      {
+         throw new IllegalArgumentException("Thread pool size must be >= 0");
+      }
+      
+      // Note: threads are created lazily so if the thread pool size
+      // here is greater than the number of threads currently in
+      // the pool, those threads will be created as they are needed
+      // hence, we do not need to adjust for increases in the pool size
+      // only for decreases
+      
+      // remove threads as necessary
+      if(mThreads.size() > size)
+      {
+         // get number of threads to remove
+         int removeCount = mThreads.size() - size;
+         
+         // iterate through threads, remove idle threads
+         Iterator i = mThreads.iterator();
+         while(i.hasNext() && removeCount > 0)
+         {
+            JobThread thread = (JobThread)i.next();
+            
+            // if thread is idle, interrupt it and remove it
+            if(thread.isIdle())
+            {
+               // interrupt thread
+               thread.interrupt();
+               
+               // remove it from the pool
+               i.remove();
+               
+               // decrement remove count
+               removeCount--;
+            }
+         }
+      }
+      
+      // set semaphore permits
+      mThreadSemaphore.setNumPermits(size);
+   }
+   
+   /**
+    * Runs the passed Runnable job on an available JobThread.
+    * 
+    * This method will lock until an available thread is acquired or
+    * the current thread is interrupted.
+    * 
+    * @param job the Runnable job to run.
+    */
+   public void runJob(Runnable job)
+   {
+      boolean permitAcquired = false;
+      
+      try
+      {
+         // acquire a thread permit
+         acquireThreadPermit();
+         
+         // permit acquired
+         permitAcquired = true;
+         
+         // run the job on an idle thread
+         runJobOnIdleThread(job);
+      }
+      catch(InterruptedException e)
+      {
+         // toggle interrupt thread to true
+         Thread.currentThread().interrupt();
+         
+         getLogger().debug(
+            "JobThreadPool: thread acquisition interrupted.");
+      }
+      
+      // if a permit was acquired, release it
+      if(permitAcquired)
+      {
+         releaseThreadPermit();
+      }
+   }
+   
+   /**
+    * Interrupts all threads in this pool and removes them.
+    */
+   public synchronized void terminateAllThreads()
+   {
+      // iterate through all threads, interrupt and remove them
+      Iterator i = mThreads.iterator();
+      while(i.hasNext())
+      {
+         JobThread thread = (JobThread)i.next();
+         
+         // interrupt thread
+         thread.interrupt();
+         
+         // remove thread
+         i.remove();
+      }      
+   }   
+   
+   /**
+    * Gets the logger for this thread pool.
+    * 
+    * @return the logger for this thread pool.
+    */
+   public Logger getLogger()
+   {
+      return LoggerManager.getLogger("dbutil");
+   }
+}
