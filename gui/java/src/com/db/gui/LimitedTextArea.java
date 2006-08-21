e462352951fb394ddcea0abc586fb95d74977d04
Dave Longley <dlongley@digitalbazaar.com>
2006-08-21 04:40:19 +0000

    Added new (unimplemented) class for a text area that has a text capacity.

---
+++ gui/java/src/com/db/gui/LimitedTextArea.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2006 Digital Bazaar, Inc.  All rights reserved.
+ */
+package com.db.gui;
+
+import javax.swing.JTextArea;
+
+/**
+ * A LimitedTextArea is a text area that can have a set text capacity. When
+ * new text is appended to this text area that would cause an overflow, the
+ * appropriate amount of text at the top of the text area will be removed.
+ * 
+ * @author Dave Longley
+ */
+public class LimitedTextArea extends JTextArea
+{
+   
+
+}
