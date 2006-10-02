dfd2e29a9835020f72b2644acd32e1f1420e036b
Dave Longley <dlongley@digitalbazaar.com>
2006-10-02 06:24:38 +0000

    Added unit test for Crc16.

---
+++ util/java/test/UTCrc16.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2006 Digital Bazaar, Inc.  All rights reserved.
+ */
+import com.db.logging.Logger;
+import com.db.logging.LoggerManager;
+import com.db.util.Crc16;
+
+/**
+ * A unit test for the Crc16.
+ * 
+ * @author Dave Longley
+ */
+public class UTCrc16
+{
+   /**
+    * Runs the unit test.
+    *
+    * @param args the arguments.
+    */
+   public static void main(String[] args)
+   {
+      try
+      {
+         // set up loggers
+         LoggerManager.setFile("dbutil", "ut-crc16.log", false);
+         LoggerManager.setConsoleVerbosity("dbutil", Logger.ERROR_VERBOSITY);
+         
+         String s = new String("hello");
+         byte[] b = s.getBytes();
+         
+         Crc16 crc16 = new Crc16();
+         crc16.update(b);
+         short c = (short)(crc16.getValue() & 0xffff);
+         System.out.println("crc=" + c);
+         System.out.println("crc hex=" + Integer.toHexString(c));
+         
+         // CRC should be 38c5 in hex
+      }
+      catch(Throwable t)
+      {
+         t.printStackTrace();
+      }
+   }
+}
