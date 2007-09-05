973465eea97a9e34f232b96cb399508c5c2364aa
Dave Longley <dlongley@digitalbazaar.com>
2007-09-05 16:42:39 +0000

    Added ByteBuffer class.

---
+++ io/cpp/db/io/ByteBuffer.cpp
@@ -0,0 +1,266 @@
+/*
+ * Copyright (c) 2007 Digital Bazaar, Inc.  All rights reserved.
+ */
+#include "db/io/ByteBuffer.h"
+
+using namespace db::io;
+
+ByteBuffer::ByteBuffer(int capacity)
+{
+   // create the byte buffer
+   mCapacity = capacity;
+   mBuffer = new char[mCapacity];
+   mOffset = 0;
+   mLength = 0;
+   mCleanup = true;
+}
+
+ByteBuffer::ByteBuffer(char* b, int offset, int length, bool cleanup)
+{
+   // set the byte buffer
+   setBytes(b, offset, length, cleanup);
+}
+
+ByteBuffer::ByteBuffer(const ByteBuffer& copy)
+{
+   // copy bytes
+   mCapacity = copy.getCapacity();
+   mBuffer = new char[mCapacity];
+   memcpy(mBuffer, copy.bytes(), copy.getCapacity());
+   mOffset = copy.offset();
+   mLength = copy.length();
+   mCleanup = true;
+}
+
+ByteBuffer::~ByteBuffer()
+{
+   // clean up byte buffer
+   cleanupBytes();
+}
+
+void ByteBuffer::cleanupBytes()
+{
+   if(mCleanup)
+   {
+      delete [] mBuffer;
+   }
+}
+
+void ByteBuffer::allocateSpace(int length, bool resize)
+{
+   if(resize)
+   {
+      // determine if the buffer needs to be resized
+      int overflow = length - available();
+      if(overflow > 0)
+      {
+         // resize the buffer by the overflow amount
+         this->resize(mCapacity + overflow);
+      }
+   }
+   
+   // determine if the data needs to be shifted
+   if(mOffset > 0)
+   {
+      int overflow = length - available() + mOffset;
+      if(overflow > 0)
+      {
+         if(mLength > 0)
+         {
+            // shift the data in the buffer
+            memcpy(mBuffer, data(), mLength);
+         }
+         
+         mOffset = 0;
+      }
+   }
+}
+
+void ByteBuffer::resize(int capacity)
+{
+   if(capacity != mCapacity)
+   {
+      // create a new buffer
+      char* newBuffer = new char[capacity];
+      
+      // copy the data into the new buffer, truncate old count as necessary
+      mCapacity = capacity;
+      mLength = (mCapacity < mLength) ? mCapacity : mLength;
+      memcpy(newBuffer, data(), mLength);
+      mOffset = 0;
+      
+      // clean up old buffer
+      cleanupBytes();
+      
+      mBuffer = newBuffer;
+   }
+}
+
+int ByteBuffer::put(const char* b, int length, bool resize)
+{
+   // allocate space for the data
+   allocateSpace(length, resize);
+   
+   // copy data into the buffer
+   length = (length < available()) ? length : available();
+   memcpy(data() + mLength, b, length);
+   mLength += length;
+   
+   return length;
+}
+
+int ByteBuffer::put(ByteBuffer& b, int length, bool resize)
+{
+   length = (length < b.length()) ? length : b.length();
+   return put(b.bytes() + b.offset(), length, resize);
+}
+
+int ByteBuffer::put(InputStream* is)
+{
+   int rval = 0;
+   
+   // if the buffer is not full, do a read
+   if(!isFull())
+   {
+      // allocate free space
+      allocateSpace(available(), false);
+      
+      // read
+      rval = is->read(data() + mLength, available());
+      if(rval != -1)
+      {
+         // increment length
+         mLength += rval;
+      }
+   }
+   
+   return rval;
+}
+
+int ByteBuffer::get(char* b, int length)
+{
+   length = (length < mLength) ? length : mLength;
+   memcpy(b, data(), length);
+   
+   // move internal pointer
+   mOffset += length;
+   mLength -= length;
+   
+   return length;
+}
+
+int ByteBuffer::get(ByteBuffer& b, int length, bool resize)
+{
+   // put data into passed buffer
+   length = (length < mLength) ? length : mLength;
+   int rval = b.put(data(), length, resize);
+   
+   // move internal pointer and change length
+   mOffset += rval;
+   mLength -= rval;
+   
+   return rval;
+}
+
+int ByteBuffer::get(OutputStream* os)
+{
+   int rval = mLength;
+   
+   os->write(data(), mLength);
+   mOffset = mLength = 0;
+   
+   return rval;
+}
+
+int ByteBuffer::clear(int length)
+{
+   int rval = mLength;
+   
+   mLength = (mLength - length > 0) ? mLength - length : 0;
+   
+   if(mLength == 0)
+   {
+      mOffset = 0;
+   }
+   else
+   {
+      mOffset += length;
+      rval = length;
+   }
+   
+   return rval;
+}
+
+int ByteBuffer::trim(int length)
+{
+   int rval = length;
+   
+   if(mLength < length)
+   {
+      rval = mLength;
+      mLength = 0;
+   }
+   else
+   {
+      mLength -= length;
+   }
+   
+   return rval;
+}
+
+int ByteBuffer::getCapacity() const
+{
+   return mCapacity;
+}
+
+void ByteBuffer::setBytes(ByteBuffer& b, bool cleanup)
+{
+   // set the byte buffer
+   setBytes(b.bytes(), b.offset(), b.length(), cleanup);
+}
+
+void ByteBuffer::setBytes(char* b, int offset, int length, bool cleanup)
+{
+   // cleanup old buffer
+   cleanupBytes();
+   
+   mBuffer = b;
+   mOffset = offset;
+   mLength = length;
+   mCleanup = cleanup;
+}
+
+char* ByteBuffer::bytes() const
+{
+   return mBuffer;
+}
+
+char* ByteBuffer::data() const
+{
+   return mBuffer + mOffset;
+}
+
+int ByteBuffer::offset() const
+{
+   return mOffset;
+}
+
+int ByteBuffer::length() const
+{
+   return mLength;
+}
+
+int ByteBuffer::available() const
+{
+   return mCapacity - mLength;
+}
+
+bool ByteBuffer::isFull() const
+{
+   return available() == 0;
+}
+
+bool ByteBuffer::isEmpty() const
+{
+   return mLength == 0;
+}
---
+++ io/cpp/db/io/ByteBuffer.h
@@ -0,0 +1,288 @@
+/*
+ * Copyright (c) 2007 Digital Bazaar, Inc.  All rights reserved.
+ */
+#ifndef db_io_ByteBuffer_H
+#define db_io_ByteBuffer_H
+
+#include "db/io/InputStream.h"
+#include "db/io/OutputStream.h"
+
+namespace db
+{
+namespace io
+{
+
+/**
+ * A ByteBuffer holds an internal array of bytes that can be dynamically
+ * resized, filled, and emptied.
+ * 
+ * @author Dave Longley
+ */
+class ByteBuffer
+{
+protected:
+   /**
+    * The internal byte buffer. 
+    */
+   char* mBuffer;
+   
+   /**
+    * The capacity for the byte buffer.
+    */
+   int mCapacity;
+   
+   /**
+    * The current offset in the buffer.
+    */
+   int mOffset;
+   
+   /**
+    * The number of valid bytes in the buffer.
+    */
+   int mLength;
+   
+   /**
+    * True if the internal buffer should be cleaned up, false if not.
+    */
+   bool mCleanup;
+   
+   /**
+    * Cleans up the internal byte buffer.
+    */
+   virtual void cleanupBytes();
+   
+public:
+   /**
+    * Creates a new ByteBuffer with the specified starting size.
+    * 
+    * @param capacity the capacity of the ByteBuffer in bytes.
+    */
+   ByteBuffer(int capacity = 0);
+   
+   /**
+    * Creates a new ByteBuffer that wraps the passed buffer of bytes.
+    * 
+    * @param b the buffer of bytes to wrap.
+    * @param offset the offset at which valid bytes begin.
+    * @param length the number of valid bytes.
+    * @param cleanup true to handle clean up of the memory, false not to.
+    */
+   ByteBuffer(char* b, int offset, int length, bool cleanup = false);
+   
+   /**
+    * Copies the data in the passed ByteBuffer into this one.
+    * 
+    * @param b the ByteBuffer to copy.
+    */
+   ByteBuffer(const ByteBuffer& copy);
+   
+   /**
+    * Destructs this ByteBuffer.
+    */
+   virtual ~ByteBuffer();
+   
+   /**
+    * Allocates enough space in the current buffer for the passed number of
+    * bytes.
+    * 
+    * @param length the number of bytes that need to be written to this buffer.
+    * @param resize true to resize the buffer as is necessary, false not to. 
+    */
+   virtual void allocateSpace(int length, bool resize);
+   
+   /**
+    * Resizes the ByteBuffer to the given capacity. Any existing data that
+    * cannot fit in the new capacity will be truncated.
+    * 
+    * @param capacity the new capacity, in bytes, for this buffer.
+    */
+   virtual void resize(int capacity);
+   
+   /**
+    * Puts data from the passed buffer into this buffer.
+    * 
+    * @param b the buffer with data to put into this buffer.
+    * @param length the number of bytes to put into this buffer.
+    * @param resize true to automatically resize this buffer if the passed
+    *               number of bytes will not otherwise fit.
+    * 
+    * @return the actual number of bytes put into this buffer, which may be
+    *         less than the number of bytes requested if this buffer is full.
+    */
+   virtual int put(const char* b, int length, bool resize);
+   
+   /**
+    * Puts data copied from the passed ByteBuffer into this buffer. The passed
+    * ByteBuffer is *not* altered.
+    * 
+    * @param b the ByteBuffer to copy data from.
+    * @param length the number of bytes to copy.
+    * @param resize true to automatically resize this buffer if the passed
+    *               number of bytes will not otherwise fit.
+    * 
+    * @return the actual number of bytes put into this buffer, which may be
+    *         less than the number of bytes requested if this buffer is full
+    *         or if the passed ByteBuffer does not have enough bytes.
+    */
+   virtual int put(ByteBuffer& b, int length, bool resize);
+   
+   /**
+    * Reads data from the passed input stream and puts it into this buffer.
+    * 
+    * This method will block until at one byte can be read from the input
+    * stream, unless this buffer is already full, or until the end of the
+    * stream has been reached.
+    * 
+    * Exception::getLast() will be set if an IO error occurs.
+    * 
+    * @param is the input stream to read from.
+    * 
+    * @return the number of bytes read from the input stream and put into
+    *         this buffer, or -1 if the end of the input stream was reached.
+    */
+   virtual int put(InputStream* is);
+   
+   /**
+    * Gets data out of this buffer and puts it into the passed buffer. This
+    * method will increment the internal pointer of this buffer by the number
+    * of bytes retrieved.
+    * 
+    * @param b the buffer to put the retrieved data into.
+    * @param length the maximum number of bytes to get.
+    * 
+    * @return the actual number of bytes retrieved, which may be 0 if this
+    *         buffer is empty.
+    */
+   virtual int get(char* b, int length);
+   
+   /**
+    * Gets data out of this buffer and puts it into the passed ByteBuffer. This
+    * method will increment the internal pointer of this buffer and the
+    * passed buffer by the number of bytes retrieved.
+    * 
+    * @param b the ByteBuffer to put the retrieved data into.
+    * @param length the maximum number of bytes to get.
+    * @param resize true to resize the passed buffer as necessary, false not to.
+    * 
+    * @return the actual number of bytes retrieved, which may be 0 if this
+    *         buffer is empty or if the passed buffer is full.
+    */
+   virtual int get(ByteBuffer& b, int length, bool resize);
+   
+   /**
+    * Gets data out of this buffer and writes it to the passed output stream.
+    * This method will increment the internal pointer of this buffer by the
+    * number of bytes retrieved.
+    * 
+    * Exception::getLast() will be set if an IO error occurs.
+    * 
+    * @param os the OutputStream to write the retrieved data to.
+    * 
+    * @return the actual number of bytes retrieved, which may be 0 if this
+    *         buffer is empty.
+    */
+   virtual int get(OutputStream* os);
+   
+   /**
+    * Clears the specified amount of data out of this buffer. If more data is
+    * requested to be cleared than there is data, all data will be cleared.
+    * 
+    * @param length the amount of data to clear from this buffer.
+    * 
+    * @return the actual amount of data cleared.
+    */
+   virtual int clear(int length = 0);
+   
+   /**
+    * Trims data from the end of this buffer without resizing it.
+    * 
+    * @param length the number of bytes to trim off the end of this buffer.
+    * 
+    * @return the actual number of bytes trimmed.
+    */
+   virtual int trim(int length);
+   
+   /**
+    * Gets the capacity of this buffer.
+    * 
+    * @return the capacity of this buffer.
+    */
+   virtual int getCapacity() const;
+   
+   /**
+    * Sets the internal buffer to the same buffer used by the passed
+    * ByteBuffer. No data is copied.
+    * 
+    * @param b the buffer to use.
+    * @param cleanup true if the passed buffer should be cleaned up by
+    *                this object, false if not.
+    */
+   virtual void setBytes(ByteBuffer& b, bool cleanup);
+   
+   /**
+    * Sets the internal buffer.
+    * 
+    * @param b the buffer to use.
+    * @param offset the offset at which the valid bytes start in the buffer.
+    * @param length the number of valid bytes in the buffer.
+    * @param cleanup true if the passed buffer should be cleaned up by
+    *                this object, false if not.
+    */
+   virtual void setBytes(char* b, int offset, int length, bool cleanup);
+   
+   /**
+    * Gets the bytes in this buffer. This method will return the underlying
+    * byte array. The offset at which valid bytes begin can be retrieved by
+    * calling offset().
+    * 
+    * @return the bytes in this buffer.
+    */
+   virtual char* bytes() const;
+   
+   /**
+    * Gets the bytes in this buffer starting at the valid offset for this
+    * buffer.
+    * 
+    * @return the bytes in this buffer starting at the valid offset.
+    */
+   virtual char* data() const;
+   
+   /**
+    * Gets the offset at which the bytes start in this buffer.
+    * 
+    * @return the offset at which the bytes start in this buffer.
+    */
+   virtual int offset() const;
+   
+   /**
+    * Gets the amount of used space in this buffer.
+    * 
+    * @return the amount of used space in this buffer.
+    */
+   virtual int length() const;
+   
+   /**
+    * Gets the amount of free space in this buffer.
+    * 
+    * @return the amount of free space in this buffer, in bytes.
+    */
+   virtual int available() const;
+   
+   /**
+    * Returns true if this buffer is full.
+    * 
+    * @return true if this buffer is full, false if it is not.
+    */
+   virtual bool isFull() const;
+   
+   /**
+    * Returns true if this buffer is empty.
+    * 
+    * @return true if this buffer is empty, false if it is not.
+    */
+   virtual bool isEmpty() const;
+};
+
+} // end namespace io
+} // end namespace db
+#endif
