77ef2ddeb900e7977debc6d8d03f995cbf8c7327
Dave Longley <dlongley@digitalbazaar.com>
2008-01-02 01:24:43 -0500

    Updated documentation.

--- data/cpp/db/data/AbstractDataFormatInspector.h
+++ data/cpp/db/data/AbstractDataFormatInspector.h
@@ -84,16 +84,20 @@ public:
     * Inspects the data in the passed buffer for some implementation
     * specific attributes. This method returns the number of bytes that
     * were successfully inspected such that the passed buffer can safely
-    * clear that number of bytes. 
+    * clear that number of bytes.
     * 
-    * An inspector should treat subsequent calls to this method as if the
-    * data in the passed buffer is consecutive in nature (i.e. read
-    * from a stream).
+    * An inspector should treat calls to this method as if the data in
+    * the passed buffer is consecutive in nature (i.e. read from a stream).
+    * 
+    * This method may return a number of bytes that is actually greater
+    * than the number passed in, if the DataInspector can determine it does
+    * not need to inspect them directly.
     * 
     * @param b the buffer with data to inspect.
-    * @param length the maximum number of bytes to inspect.
+    * @param length the number of bytes in the buffer.
     * 
-    * @return the number of bytes that were inspected in the passed buffer.
+    * @return the number of bytes inspected by this DataInspector and that
+    *         should not be passed to it again.
     */
    virtual int inspectData(const char* b, int length);
    
