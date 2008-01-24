7b79ec417df0e15af06a0e738caea93319bf3bfb
David I. Lehn <dlehn@digitalbazaar.com>
2008-01-24 12:54:45 -0500

    Add DLL support for exported category vars.

--- logging/cpp/db/logging/LoggingCategories.h
+++ logging/cpp/db/logging/LoggingCategories.h
@@ -9,19 +9,35 @@
 // This is a list of pre-defined logging categories that can be used by any
 // module that would like to log messages to a specific category.
 
-extern db::logging::Category* DB_CONFIG_CAT;
-extern db::logging::Category* DB_CRYPTO_CAT; 
-extern db::logging::Category* DB_DATA_CAT;
-extern db::logging::Category* DB_EVENT_CAT;
-extern db::logging::Category* DB_GUI_CAT;
-extern db::logging::Category* DB_IO_CAT;
-extern db::logging::Category* DB_LOGGING_CAT;
-extern db::logging::Category* DB_MAIL_CAT;
-extern db::logging::Category* DB_MODEST_CAT;
-extern db::logging::Category* DB_NET_CAT;
-extern db::logging::Category* DB_RT_CAT;
-extern db::logging::Category* DB_SQL_CAT;
-extern db::logging::Category* DB_UTIL_CAT;
+#ifndef __GNUC__
+# define __DLL_IMPORT __declspec(dllimport)
+#else
+# define __DLL_IMPORT __attribute__((dllimport)) extern
+#endif
+
+#if defined (BUILD_LOGGING_DLL) || !defined (__WIN32__)
+# define DLL_IMPORT extern
+#else
+# define DLL_IMPORT __DLL_IMPORT
+#endif
+
+DLL_IMPORT db::logging::Category* DB_ALL_CAT;
+DLL_IMPORT db::logging::Category* DB_CONFIG_CAT;
+DLL_IMPORT db::logging::Category* DB_CRYPTO_CAT; 
+DLL_IMPORT db::logging::Category* DB_DATA_CAT;
+DLL_IMPORT db::logging::Category* DB_EVENT_CAT;
+DLL_IMPORT db::logging::Category* DB_GUI_CAT;
+DLL_IMPORT db::logging::Category* DB_IO_CAT;
+DLL_IMPORT db::logging::Category* DB_LOGGING_CAT;
+DLL_IMPORT db::logging::Category* DB_MAIL_CAT;
+DLL_IMPORT db::logging::Category* DB_MODEST_CAT;
+DLL_IMPORT db::logging::Category* DB_NET_CAT;
+DLL_IMPORT db::logging::Category* DB_RT_CAT;
+DLL_IMPORT db::logging::Category* DB_SQL_CAT;
+DLL_IMPORT db::logging::Category* DB_UTIL_CAT;
+
+#undef DLL_IMPORT
+#undef __DLL_IMPORT
 
 namespace db
 {
