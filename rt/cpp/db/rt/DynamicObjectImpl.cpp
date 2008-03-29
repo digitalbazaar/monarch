5496777d49656088db9b7e20a17e7432cbf795df
Dave Longley <dlongley@digitalbazaar.com>
2008-03-29 14:08:13 -0400

    Added function to get enums -- which is equivalent to getting int32s.

--- rt/cpp/db/rt/DynamicObjectImpl.cpp
+++ rt/cpp/db/rt/DynamicObjectImpl.cpp
@@ -507,6 +507,11 @@ double DynamicObjectImpl::getDouble()
    return rval;
 }
 
+int DynamicObjectImpl::getEnum()
+{
+   return getInt32();
+}
+
 bool DynamicObjectImpl::hasMember(const char* name)
 {
    bool rval = false;
