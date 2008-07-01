da33e291f051ba2664356679e3336c8a332c2a1a
Dave Longley <dlongley@digitalbazaar.com>
2008-07-01 17:41:21 -0400

    Moved content length variable into HttpTrailer base class.

--- net/cpp/db/net/http/HttpTrailer.cpp
+++ net/cpp/db/net/http/HttpTrailer.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007 Digital Bazaar, Inc.  All rights reserved.
+ * Copyright (c) 2007-2008 Digital Bazaar, Inc.  All rights reserved.
  */
 #include "db/net/http/HttpTrailer.h"
 
@@ -7,6 +7,7 @@ using namespace db::net::http;
 
 HttpTrailer::HttpTrailer()
 {
+   mContentLength = 0;
 }
 
 HttpTrailer::~HttpTrailer()
@@ -15,4 +16,11 @@ HttpTrailer::~HttpTrailer()
 
 void HttpTrailer::update(unsigned long long contentLength)
 {
+   // save content length
+   mContentLength = contentLength;
+}
+
+unsigned long long HttpTrailer::getContentLength()
+{
+   return mContentLength;
 }
