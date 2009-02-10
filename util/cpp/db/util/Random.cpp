a214ea79ec38c91c1dccef30599b6e5b07818ae8
Dave Longley <dlongley@digitalbazaar.com>
2009-02-10 16:45:39 -0500

    Added code for random on windows.

--- util/cpp/db/util/Random.cpp
+++ util/cpp/db/util/Random.cpp
@@ -14,16 +14,26 @@ using namespace db::util;
 void Random::seed()
 {
    uint64_t time = System::getCurrentMilliseconds();
-   srand((unsigned int)(time & 0xFFFFFFFF));
+   Random::seed((unsigned int)(time & 0xFFFFFFFF));
 }
 
 void Random::seed(unsigned int value)
 {
+#ifndef WIN32
    srandom(value);
+#endif
+   // Note: Windows does not need/use seeding.
 }
 
 uint64_t Random::next(uint64_t low, uint64_t high)
 {
+#ifdef WIN32
+   unsigned int num;
+   rand_s(&num);
+   // get a random number between 1 and 1000000000
+   return low + (uint64_t)((long double)high * (num / (RAND_MAX + 1.0)));
+#else
    // get a random number between 1 and 1000000000
    return low + (uint64_t)((long double)high * (random() / (RAND_MAX + 1.0)));
+#endif
 }
