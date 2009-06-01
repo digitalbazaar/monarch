54c17091602feece59140164065e1a73c21cad2b
Dave Longley <dlongley@digitalbazaar.com>
2009-06-01 15:33:26 -0400

    Made Server a DLL_CLASS to fix linking issues.

--- net/cpp/db/net/Server.h
+++ net/cpp/db/net/Server.h
@@ -20,6 +20,16 @@ namespace db
 namespace net
 {
 
+#ifdef WIN32
+#   ifdef BUILD_DB_NET_DLL
+#      define DLL_CLASS __WIN32_DLL_EXPORT
+#   else
+#      define DLL_CLASS __WIN32_DLL_IMPORT
+#   endif
+#else
+#   define DLL_CLASS
+#endif
+
 /**
  * A Server communicates by using Connections that are established on
  * ports that this server listens to or by using Datagrams received on
@@ -30,7 +40,7 @@ namespace net
  * 
  * @author Dave Longley
  */
-class Server
+class DLL_CLASS Server
 {
 public:
    /**
