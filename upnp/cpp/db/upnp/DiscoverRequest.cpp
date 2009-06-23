3736423b4df75c8dde600cb5fd5576665533c0d0
Dave Longley <dlongley@digitalbazaar.com>
2009-06-23 13:29:50 -0400

    Started dbupnp C++ library.

---
+++ upnp/cpp/db/upnp/DiscoverRequest.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2009 Digital Bazaar, Inc. All rights reserved.
+ */
+#include "db/upnp/DiscoverRequest.h"
+
+using namespace db::upnp;
+
+DiscoverRequest::DiscoverRequest()
+{
+}
+DiscoverRequest::~DiscoverRequest()
+{
+}
---
+++ upnp/cpp/db/upnp/DiscoverRequest.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2009 Digital Bazaar, Inc. All rights reserved.
+ */
+#ifndef db_upnp_DiscoverRequest_H
+#define db_upnp_DiscoverRequest_H
+
+namespace db
+{
+namespace upnp
+{
+
+/**
+ * FIXME
+ * 
+ * @author Dave Longley
+ */
+class DiscoverRequest
+{
+public:
+   /**
+    * Creates a new DiscoverRequest.
+    */
+   DiscoverRequest();
+   
+   /**
+    * Destructs this DiscoverRequest.
+    */
+   virtual ~DiscoverRequest();
+};
+
+} // end namespace upnp
+} // end namespace db
+#endif
