e0ceab76016708544bd1f3c644519a3b2344222e
Dave Longley <dlongley@digitalbazaar.com>
2009-12-09 15:07:26 -0500

    Changed hash function to something good.

--- cpp/tests/test-hashtable.cpp
+++ cpp/tests/test-hashtable.cpp
@@ -56,6 +56,78 @@ struct JavaHashCodeAsHash
    }
 };
 
+/* Grabbed from: http://www.azillionmonkeys.com/qed/hash.html
+ *
+ * Look into licensing issues if we use this for more than just testing.
+ */
+#undef get16bits
+#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
+  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
+#define get16bits(d) (*((const uint16_t *)(d)))
+#endif
+
+#if !defined (get16bits)
+#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
+                       +(uint32_t)(((const uint8_t *)(d))[0]) )
+#endif
+struct SuperFastHash
+{
+   int operator()(int key) const
+   {
+      const char* data = (const char*)&key;
+      int len = sizeof(int);
+      int hash = len;
+      int tmp;
+      int rem;
+
+      if(len <= 0 || data == NULL) return 0;
+
+      rem = len & 3;
+      len >>= 2;
+
+      /* Main loop */
+      for(; len > 0; len--)
+      {
+         hash += get16bits(data);
+         tmp = (get16bits(data + 2) << 11) ^ hash;
+         hash = (hash << 16) ^ tmp;
+         data += 2 * sizeof(uint16_t);
+         hash += hash >> 11;
+      }
+
+      /* Handle end cases */
+      switch(rem)
+      {
+         case 3:
+            hash += get16bits (data);
+            hash ^= hash << 16;
+            hash ^= data[sizeof (uint16_t)] << 18;
+            hash += hash >> 11;
+            break;
+         case 2:
+            hash += get16bits (data);
+            hash ^= hash << 11;
+            hash += hash >> 17;
+            break;
+         case 1:
+            hash += *data;
+            hash ^= hash << 10;
+            hash += hash >> 1;
+            break;
+      }
+
+      /* Force "avalanching" of final 127 bits */
+      hash ^= hash << 3;
+      hash += hash >> 5;
+      hash ^= hash << 4;
+      hash += hash >> 17;
+      hash ^= hash << 25;
+      hash += hash >> 6;
+
+      return hash;
+   }
+};
+
 struct StringEquals
 {
    bool operator()(const char* key1, const char* key2)
@@ -215,14 +287,14 @@ class HashMash : public HashMashBase
 {
 protected:
    map<_K, _V>* mMap;
-   HashTable<_K, _V, KeyAsHash>* mHT;
+   HashTable<_K, _V, SuperFastHash>* mHT;
    ExclusiveLock* mExclusiveLock;
    SharedLock* mSharedLock;
 public:
    HashMash(
       uint32_t loops, uint32_t slots, uint32_t writes, uint32_t reads,
       map<_K, _V>* m,
-      HashTable<_K, _V, KeyAsHash>* h,
+      HashTable<_K, _V, SuperFastHash>* h,
       ExclusiveLock* exclusiveLock,
       SharedLock* sharedLock) :
       HashMashBase(loops, slots, writes, reads),
@@ -525,7 +597,7 @@ void runHashTableVsMapTest(
    tr.test(name);
    {
       _hashMashInfo(comment, "HashTable<int, uint32_t>");
-      HashTable<int, uint32_t, KeyAsHash> h(initialSize);
+      HashTable<int, uint32_t, SuperFastHash> h(initialSize);
       Thread* t[threads];
       HashMashBase* mashers[threads];
       for(uint32_t i = 0; i < threads; i++)
