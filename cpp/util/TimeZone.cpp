c526c42be217261f9c5bb161262c6a89eed07597
Dave Longley <dlongley@digitalbazaar.com>
2010-05-05 17:15:34 -0400

    Changed timezone string to case-insensitive comparison.

--- cpp/util/TimeZone.cpp
+++ cpp/util/TimeZone.cpp
@@ -28,26 +28,26 @@ TimeZone TimeZone::getTimeZone(const char* tz)
 {
    TimeZone rval;
 
-   if(strcmp(tz, "GMT") == 0 || strcmp(tz, "UTC") == 0)
+   if(strcasecmp(tz, "GMT") == 0 || strcasecmp(tz, "UTC") == 0)
    {
       rval.mMinutesWest = 0;
    }
-   else if(strcmp(tz, "EDT") == 0)
+   else if(strcasecmp(tz, "EDT") == 0)
    {
       // 4 hours west
       rval.mMinutesWest = 240;
    }
-   else if(strcmp(tz, "EST") == 0)
+   else if(strcasecmp(tz, "EST") == 0)
    {
       // 5 hours west
       rval.mMinutesWest = 300;
    }
-   else if(strcmp(tz, "PDT") == 0)
+   else if(strcasecmp(tz, "PDT") == 0)
    {
       // 7 hours west
       rval.mMinutesWest = 420;
    }
-   else if(strcmp(tz, "PST") == 0)
+   else if(strcasecmp(tz, "PST") == 0)
    {
       // 8 hours west
       rval.mMinutesWest = 480;
