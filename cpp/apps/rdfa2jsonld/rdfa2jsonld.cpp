38c2e177ace304c9fd525809134d009f8676006b
Dave Longley <dlongley@digitalbazaar.com>
2010-12-13 18:56:00 -0500

    Added rdfa2jsonld app.

---
+++ cpp/apps/rdfa2jsonld/rdfa2jsonld.cpp
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2010 Digital Bazaar, Inc. All rights reserved.
+ */
+#include "monarch/app/AppFactory.h"
+#include "monarch/data/json/JsonWriter.h"
+#include "monarch/data/rdfa/RdfaReader.h"
+#include "monarch/io/FileInputStream.h"
+
+#include <cstdio>
+
+using namespace std;
+using namespace monarch::app;
+using namespace monarch::config;
+using namespace monarch::data::json;
+using namespace monarch::data::rdfa;
+using namespace monarch::io;
+using namespace monarch::modest;
+using namespace monarch::rt;
+
+#define APP_NAME "monarch.apps.rdfa2jsonld.Rdfa2JsonLd"
+
+namespace monarch
+{
+namespace apps
+{
+namespace rdfa2jsonld
+{
+
+static bool _processFile(const char* inFile, const char* baseUri)
+{
+   bool rval;
+
+   File file(inFile);
+   FileInputStream fis(file);
+   RdfaReader reader;
+   reader.setBaseUri(baseUri);
+   DynamicObject dyno;
+   rval = reader.start(dyno) && reader.read(&fis) && reader.finish();
+   fis.close();
+
+   if(rval)
+   {
+      printf("RDFa to JSON-LD: '%s'\n", file->getAbsolutePath());
+      JsonWriter::writeToStdOut(dyno, false, false);
+   }
+
+   return rval;
+}
+
+class Rdfa2JsonLdApp : public App
+{
+public:
+   Rdfa2JsonLdApp() {};
+   virtual ~Rdfa2JsonLdApp() {};
+   virtual DynamicObject getCommandLineSpec(Config& cfg)
+   {
+      // initialize config
+      Config& c = cfg[ConfigManager::MERGE][APP_NAME];
+      c["baseUri"] = "http://example.org";
+
+      DynamicObject spec;
+      spec["help"] =
+"PortMap Options\n"
+"      --base-uri      The base URI to use.\n"
+"\n";
+
+      DynamicObject opt(NULL);
+
+      // create option to set base URI
+      opt = spec["options"]->append();
+      opt["long"] = "--base-uri";
+      opt["argError"] = "Base URI must be a string.";
+      opt["arg"]["root"] = c;
+      opt["arg"]["path"] = "baseUri";
+
+      // use extra options as files to process
+      opt = spec["options"]->append();
+      opt["extra"]["root"] = c;
+      opt["extra"]["path"] = "files";
+
+      return spec;
+   };
+
+   /**
+    * Runs the app.
+    *
+    * @return true if successful, false if an exception occurred.
+    */
+   virtual bool run()
+   {
+      Config cfg = getConfig()[APP_NAME];
+
+      // process files
+      DynamicObject& files = cfg["files"];
+      if(files->length() == 0)
+      {
+         ExceptionRef e = new Exception(
+            "No files specified.",
+            APP_NAME ".NoFiles");
+         Exception::set(e);
+      }
+      else
+      {
+         const char* baseUri = cfg["baseUri"];
+         DynamicObjectIterator i = files.getIterator();
+         bool success = true;
+         while(success && i->hasNext())
+         {
+            success = _processFile(i->next()->getString(), baseUri);
+         }
+      }
+
+      return !Exception::isSet();
+   };
+};
+
+class Rdfa2JsonLdAppFactory : public AppFactory
+{
+public:
+   Rdfa2JsonLdAppFactory() : AppFactory(APP_NAME, "1.0") {}
+   virtual ~Rdfa2JsonLdAppFactory() {}
+   virtual App* createApp()
+   {
+      return new Rdfa2JsonLdApp();
+   }
+};
+
+} // end namespace rdfa2jsonld
+} // end namespace apps
+} // end namespace monarch
+
+Module* createModestModule()
+{
+   return new monarch::apps::rdfa2jsonld::Rdfa2JsonLdAppFactory();
+}
+
+void freeModestModule(Module* m)
+{
+   delete m;
+}
